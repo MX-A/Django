6166825de2ba1e297806767cf70f9cf77ee734ff
MX-A-S821 <15738512330@163.com>
2019-09-22 14:22:14 +0800

    数据测试

---
+++ testpy.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+stnum ="250"
+print(int(stnum))
+
+
+# s = []
+# X=np.arange(0,100)
+# for i in range(0,100):
+#     num=random.randint(10,100)
+#     s.append(num)
+#     print(s)
+# plt.plot(X,s)
+# plotpath = r'H:\django-test\mytest\static\fig.png'
+# plt.savefig(plotpath)
+
